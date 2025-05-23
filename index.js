fd8de3d444a58ae40abdff4cb9879a0de7aaf809
Anas Younus <anusyounus@gmail.com>
2025-05-22 22:54:41 -0700

    save changes

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 import express from "express"
+import { AdminRoutes } from "./routes/AdminRoutes.js"
+import "dotenv/config"
+import mongoose from "mongoose"
 
 
 const app = express()
+const PORT = process.env.PORT
 
-app.use("/", (req, res) => {
+app.use(express.json())
+app.use(express.urlencoded())
+
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log("MongoDB connected")
+})
+.catch(() => {
+    console.log("MongoDB Disconnected")
+})
+
+app.get("/", (req, res) => {
     res.send("Hello")
 })
 
-app.listen(3000, () => {
-    console.log("Server is listeinig at http://localhost:3000")
+app.use("/admin", AdminRoutes)
+app.listen(PORT, () => {
+    console.log(`Server is listeinig at http://localhost:${PORT}`)
 })
